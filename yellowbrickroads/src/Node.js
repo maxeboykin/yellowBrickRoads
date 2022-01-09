075144177c765a9843a303a4b0fe2962724557b6
Maxwell Boykin <maxericboykin@gmail.com>
2022-01-08 20:01:11 -0500

    newNodeComponent

---
+++ yellowbrickroads/src/Node.js
@@ -0,0 +1,9 @@
+import React, {useEffect, useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+
+const node = () => {
+
+
+}
+
+export default node;
--- yellowbrickroads/src/yellowBrickRoad.js
+++ yellowbrickroads/src/yellowBrickRoad.js
@@ -1,9 +1,15 @@
 import React, {useEffect, useState} from "react";
 import {useDispatch, useSelector} from "react-redux";
+import Node from './Node'
 
 
 const yellowBrickRoad = () => {
 
+  return (
+    <div>
+      <Node></Node>
+    </div>
+  )
 }
 
 export default yellowBrickRoad;
