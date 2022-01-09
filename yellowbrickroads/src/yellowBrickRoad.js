91e157662e32cec5c577f0afcb3cd859a44eb0cc
Maxwell Boykin <maxericboykin@gmail.com>
2022-01-08 21:17:56 -0500

    AllNodes

--- yellowbrickroads/src/yellowBrickRoad.js
+++ yellowbrickroads/src/yellowBrickRoad.js
@@ -3,13 +3,31 @@ import {useDispatch, useSelector} from "react-redux";
 import Node from './Node'
 
 
-const yellowBrickRoad = () => {
+const YellowBrickRoad = () => {
+
+  const [nodes, setNode] = useState([]);
+  useEffect(() => {
+    const nodes = [];
+    for(let row = 0; row < 15; row++){
+      const currentRow = [];
+      for(let col = 0; col < 50; col++){
+        currentRow.push([]);
+      }
+      nodes.push(currentRow);
+    }
+  }, []);
+
 
   return (
     <div>
-      <Node></Node>
+      {nodes.map((row, index) => {
+        return <div>
+          {row.map((node, index) => <Node></Node>)}
+          </div>
+      })}
+      <Node>Foo</Node>
     </div>
   )
 }
 
-export default yellowBrickRoad;
+export default YellowBrickRoad;
